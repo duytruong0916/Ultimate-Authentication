8e86f2409dafbd2b8b110c0bf4e10b35db8bb951
Duy Truong <duytruong169@gmail.com>
2020-01-09 18:07:29 -0600

    new

---
+++ server/controllers/auth.js
@@ -0,0 +1,6 @@
+
+
+exports.signup = (req,res)=>{
+    console.log('You hit the signup endpoint')
+    res.json({data: 'you hit the signup endpoint'})
+}
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=> console.log("DATABASE connected"))
+  .catch(err => console.log('DATABASE connect error:', err));
+const app = express();
+const authRouters = require('../server/routes/auth');
+
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+if((process.env.NODE_ENV = 'development')) {
+    app.use(cors({origin: `http://localhost:3000`}));
+};
+//app.use(cors());
+app.use('/api', authRouters);
+const port  = process.env.PORT || 8000;
+
+
+app.listen(port, ()=>{
+    console.log(`App is running on port ${port}- ${process.env.NODE_ENV}`)
+})
